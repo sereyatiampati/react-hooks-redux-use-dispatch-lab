fe52da9d9fcc72424ca5ca16d6ecf8d6de5665be
Sereya Tiampati <sereyatiampatistudies@gmail.com>
2023-04-10 18:27:01 +0300

    done

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { legacy_createStore as createStore } from "redux";
 
 import restaurantsReducer from "./features/restaurant/restaurantsSlice";
 
-const store = createStore(restaurantsReducer);
+const store = createStore(restaurantsReducer, 
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
   <Provider store={store}>
